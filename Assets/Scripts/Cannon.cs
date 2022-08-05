be595e187fabfa55a39d37ba5c26e8effb5ac902
amurray56 <87687643+amurray56@users.noreply.github.com>
2022-08-05 11:21:24 +1000

    Cannon Shot Scripts
    Working on cannonball shooting scripts

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public GameObject cannonBall;
+    public GameObject barrelEnd;
+    private Rigidbody rb;
+    private Vector3 worldPosition;
+    private float timer;
+    public float timeBetweenShots = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(Input.GetMouseButtonDown(0) && timer >= timeBetweenShots)
+        {
+            Instantiate(cannonBall, barrelEnd.transform.position, barrelEnd.transform.rotation);
+            timer = 0f;
+        }
+
+        Vector3 mousePos = Input.mousePosition;
+        worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 30f));
+        Quaternion direction = Quaternion.LookRotation(worldPosition);
+        rb.MoveRotation(direction);
+    }
+}
---
+++ Assets/Scripts/CannonBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float force = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+        rb.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
