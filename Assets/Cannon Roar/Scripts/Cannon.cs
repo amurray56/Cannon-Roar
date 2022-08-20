65559fd6413fddc6d2e800664efcc8863b2c18c0
amurray56 <87687643+amurray56@users.noreply.github.com>
2022-08-20 15:22:24 +1000

    Maybe the cannon works?

--- Assets/Cannon Roar/Scripts/Cannon.cs
+++ Assets/Cannon Roar/Scripts/Cannon.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using Liminal.SDK.VR;
 using Liminal.SDK.VR.Input;
-using Liminal.SDK.VR.Pointers;
-using UnityEngine.XR;
+using Liminal.SDK.VR.Avatars.Controllers;
 
-public class Cannon : MonoBehaviour
+public class Cannon : MonoBehaviour, IPointerClickHandler, IEventSystemHandler
 {
     [SerializeField]
     private GameObject cannonBall;
@@ -23,31 +22,48 @@ public class Cannon : MonoBehaviour
     [SerializeField]
     private Transform cBase;
 
-    IVRPointer vRPointer;
+    public GameObject reticle;
+    private bool grabHandle;
+    private IVRControllerVisual vRControllerVisual;
 
     // Start is called before the first frame update
     void Start()
     {
+        grabHandle = false;
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var primaryInput = VRDevice.Device.PrimaryInputDevice;
+        IVRInputDevice primaryInput = VRDevice.Device.PrimaryInputDevice;
 
         timer += Time.deltaTime;
-        if(Input.GetMouseButtonDown(0) && timer >= timeBetweenShots || primaryInput.GetButtonDown(VRButton.Trigger) && timer >= timeBetweenShots)
+        if(Input.GetMouseButtonDown(0) && timer >= timeBetweenShots && grabHandle || primaryInput.GetButtonDown(VRButton.Trigger) && timer >= timeBetweenShots && grabHandle)
         {
             Instantiate(cannonBall, barrelEnd.transform.position, barrelEnd.transform.rotation);
             timer = 0f;
         }
 
         
-        mousePos = Input.mousePosition;
-        worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, -1000));
-        cBase.LookAt(new Vector3(worldPosition.x, 0, worldPosition.z));
-        cannon.LookAt(worldPosition);
+        if (grabHandle)
+        {
+            //vRControllerVisual = GameObject.Find("VRAvatar").GetComponentInChildren<IVRControllerVisual>();
+            mousePos = reticle.transform.position; //vRControllerVisual.transform.TransformDirection(vRControllerVisual.transform.position);
+            worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, -1000));
+            cBase.LookAt(new Vector3(worldPosition.x, 0, worldPosition.z));
+            cannon.LookAt(worldPosition);
+        }
+        
+    }
 
+    
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == 0 && eventData.pointerEnter.name == "handle")
+        {
+            grabHandle = !grabHandle;
+        }
     }
 }
