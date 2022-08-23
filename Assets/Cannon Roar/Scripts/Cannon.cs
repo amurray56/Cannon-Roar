840697ffbac37d91b3bedd1ba69b813c3221b5eb
amurray56 <87687643+amurray56@users.noreply.github.com>
2022-08-23 16:38:49 +1000

    adjusted controller y axis

--- Assets/Cannon Roar/Scripts/Cannon.cs
+++ Assets/Cannon Roar/Scripts/Cannon.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using Liminal.SDK.VR;
 using Liminal.SDK.VR.Input;
+using Liminal.SDK.VR.Avatars;
 
 public class Cannon : MonoBehaviour, IPointerClickHandler, IEventSystemHandler
 {
@@ -11,11 +12,9 @@ public class Cannon : MonoBehaviour, IPointerClickHandler, IEventSystemHandler
     private GameObject cannonBall;
     [SerializeField]
     private GameObject barrelEnd;
-    private Rigidbody rb;
     private Vector3 worldPosition;
     private float timer;
     public float timeBetweenShots = 1.5f;
-    private Vector3 mousePos;
     [SerializeField]
     private Transform cannon;
     [SerializeField]
@@ -24,15 +23,15 @@ public class Cannon : MonoBehaviour, IPointerClickHandler, IEventSystemHandler
     private new ParticleSystem particleSystem;
     private new AudioSource audio;
 
-    public GameObject reticle;
     private bool grabHandle;
     private Transform vrController;
+
+    VRAvatarLimb limb;
 
     // Start is called before the first frame update
     void Start()
     {
         grabHandle = false;
-        rb = GetComponent<Rigidbody>();
         particleSystem = GetComponentInChildren<ParticleSystem>();
         audio = GetComponent<AudioSource>();
     }
@@ -43,38 +42,33 @@ public class Cannon : MonoBehaviour, IPointerClickHandler, IEventSystemHandler
         IVRInputDevice primaryInput = VRDevice.Device.PrimaryInputDevice;
 
         timer += Time.deltaTime;
-        if(Input.GetMouseButtonDown(0) && timer >= timeBetweenShots && grabHandle || primaryInput.GetButtonDown(VRButton.Trigger) && timer >= timeBetweenShots && grabHandle)
+        if (Input.GetMouseButtonDown(0) && timer >= timeBetweenShots && grabHandle || primaryInput.GetButtonDown(VRButton.Trigger) && timer >= timeBetweenShots && grabHandle)
         {
             Instantiate(cannonBall, barrelEnd.transform.position, barrelEnd.transform.rotation);
             particleSystem.Play();
             audio.Play();
             timer = 0f;
         }
-        
+
         if (grabHandle)
         {
-            //vRControllerVisual = GameObject.Find("VRAvatar").GetComponentInChildren<IVRControllerVisual>();
-            //mousePos = reticle.transform.position; //vRControllerVisual.transform.TransformDirection(vRControllerVisual.transform.position);
-            //worldPosition = reticle.transform.position + reticle.transform.forward * -1000; //Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, mousePos.z));
-            //cBase.LookAt(new Vector3(worldPosition.x, 0, worldPosition.z));
-            //cannon.LookAt(worldPosition);
-
             vrController = GameObject.Find("PrimaryHand").transform;
             worldPosition = vrController.transform.position + vrController.transform.forward * -1000;
             cBase.LookAt(new Vector3(worldPosition.x, 0, worldPosition.z));
-            cannon.LookAt(worldPosition);
-
+            cannon.LookAt(new Vector3(worldPosition.x, worldPosition.y + 500, worldPosition.z));
         }
 
     }
 
-    
+
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        if(eventData.button == 0 && eventData.pointerEnter.name == "handle")
+        if (eventData.button == 0 && eventData.pointerEnter.name == "handle")
         {
             grabHandle = !grabHandle;
         }
     }
+
+    
 }
