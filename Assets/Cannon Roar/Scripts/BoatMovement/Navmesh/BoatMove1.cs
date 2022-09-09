77d4b77ae09884992a985fa328eb3999ca28a697
Steven Roache <99855707+Roache24@users.noreply.github.com>
2022-09-09 15:30:27 +1000

    Update
    Friday Work
    - Working on Navmesh Movement
    - Making the Boat Rotate like a boat

--- Assets/Cannon Roar/Scripts/BoatMovement/Navmesh/BoatMove1.cs
+++ Assets/Cannon Roar/Scripts/BoatMovement/Navmesh/BoatMove1.cs
@@ -8,18 +8,19 @@ public class BoatMove1 : MonoBehaviour
     //Stores a reference to the waypoint system that the boat will use
     [SerializeField] private Waypoint1 waypointScript;
 
-    [SerializeField] private float moveSpeed = 20f, disThreshold = 1f ;
+    [SerializeField] private float disThreshold = 1f ;
 
     private Transform currentWaypoint;
 
-    Rigidbody rBody;
+    private Transform boat;
 
+    NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
         //Sets rigidBody
-        rBody = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
 
         //Set Initial position to first waypoint
         currentWaypoint = waypointScript.NextWaypoint(currentWaypoint);
@@ -27,27 +28,23 @@ public class BoatMove1 : MonoBehaviour
 
         //Sets the next target waypoint
         currentWaypoint = waypointScript.NextWaypoint(currentWaypoint);
-        transform.LookAt(currentWaypoint);
+       
 
     }
 
     private void FixedUpdate()
     {
-        Vector3 direction = (currentWaypoint.position - transform.position).normalized;
 
-        rBody.MovePosition(transform.position + direction * moveSpeed * Time.deltaTime);
+        agent.SetDestination(currentWaypoint.position);
 
         if (Vector3.Distance(transform.position, currentWaypoint.position) < disThreshold)
         {
             currentWaypoint = waypointScript.NextWaypoint(currentWaypoint);
         }
 
-        transform.LookAt(currentWaypoint);
-
+        currentWaypoint = waypointScript.NextWaypoint(currentWaypoint);
+        Vector3.RotateTowards(boat.position, currentWaypoint.position, 6.83f, float.PositiveInfinity);
 
     }
-
-
    
-
 }
