bbb2f0165f1a6563c44d2654439f785448e508e0
amurray56 <87687643+amurray56@users.noreply.github.com>
2022-11-11 09:54:52 +1100

    working on enemy shooting

---
+++ Assets/Cannon Roar/Scripts/EnemyShooting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDrawGizmos()
+    {
+        foreach (Transform target in gameObject.GetComponentInChildren<Transform>())
+        {
+            if (target.CompareTag("Target"))
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawSphere(target.transform.position, 1);
+            }
+        }
+    }
+}
