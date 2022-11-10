b7877841a2d3dea67ed3da7db183265c6346b0c7
Steven Roache <99855707+Roache24@users.noreply.github.com>
2022-11-11 10:52:23 +1100

    StartOnSpawner
    Just a start on the spawner scritps
    Minor folder restructure

---
+++ Assets/Cannon Roar/Scripts/SteveScripts/BoatMovement/Patrol.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour
+{
+    //Stores a reference to the waypoint system that the boat will use
+    [SerializeField] private PatrolWaypoint waypointScript;
+
+    [SerializeField] private float disThreshold = 1f ;
+
+    private Transform currentWaypoint;
+
+    NavMeshAgent agent;
+
+    EnemyHealth enemyHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Sets rigidBody
+        agent = GetComponent<NavMeshAgent>();
+
+        enemyHealth = GetComponent<EnemyHealth>();
+
+        //Set Initial position to first waypoint
+        currentWaypoint = waypointScript.NextWaypoint(currentWaypoint);
+        transform.position = currentWaypoint.position;
+
+        //Sets the next target waypoint
+        currentWaypoint = waypointScript.NextWaypoint(currentWaypoint);
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        if(enemyHealth.health == 0)
+        {
+            return;
+        }
+        else
+        {
+            agent.SetDestination(currentWaypoint.position);
+
+            if (Vector3.Distance(transform.position, currentWaypoint.position) < disThreshold)
+            {
+                currentWaypoint = waypointScript.NextWaypoint(currentWaypoint);
+            }
+        }
+       
+    }
+   
+}
---
+++ Assets/Cannon Roar/Scripts/SteveScripts/Spawner/SpawnerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{ 
+    public float spawnTime;
+    public GameObject[] enemyPrefabs;
+
+    //Holds the max number of enemies alloweed at anyone time
+    public int maxNumberOfEnemiesAtOneTime = 1;
+    public int maxNumberOfEnemiesOtherSpawnerLifeTime = 5;
+
+    //Lists
+    private List<Transform> spawnPoints = new List<Transform>();    //lists for spawnpoints
+    [HideInInspector]
+    public List<Transform> waypoints = new List<Transform>();       //list for waypoints
+    //List for enemies this spawner has created that are still alive
+    [HideInInspector] //hides variable below
+    public List<GameObject> enemiesFromThisSpawnerList = new List<GameObject>();
+
+    //General
+    private int enemycount;
+    private bool enableSpawner = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
