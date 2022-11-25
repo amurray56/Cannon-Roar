dc078850943a85d419771a0f8c89a232baee3a5e
Steven Roache <99855707+Roache24@users.noreply.github.com>
2022-11-25 12:35:31 +1100

    SandHit Implemented
    -Enemy CannonBalls trigger the sandhit particle effect.

---
+++ Assets/Cannon Roar/Scripts/Enemies/SandImpact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandImpact : MonoBehaviour
+{
+    ParticleSystem sandHit;
+    
+    void Start()
+    {
+        sandHit = transform.GetChild(0).GetComponent<ParticleSystem>();
+    }
+
+    private void OnTriggerEnter(Collider trigger)
+    {
+        if (trigger.CompareTag("EnemyCannonBall"))
+        {
+            sandHit.Play();
+        }
+    }
+}
